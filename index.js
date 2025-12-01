bed2833d2c2b0ac3231db933d213093d961d97ed
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-02 02:59:29 +0530

    Add MongoDB connection and student schema for POST requests😒

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 let app = express();
 
 app.use(bodyParser.json());  //idhu app = express in pirahu koduttal wendum aduththa requests(GET,POST,PUT,DELETE) nadakka mun
+
+let mongoUrl = "mongodb+srv://admin:123@cluster0.hjazmey.mongodb.net/?appName=Cluster0"
+
+mongoose.connect(mongoUrl);
+
+let connection =  mongoose.connection
+connection.once("open", ()=>{
+    console.log("MongoDB connected successfully")
+});
+
 
 app.get("/", (req, res)=>{
     console.log( "That is a GET request!")
@@ -13,8 +24,31 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/", (req, res)=>{
-    console.log("This is a POST request")
-    res.json( {message : "This is a POST request", status : "Success" } )
+    let studentSchema = mongoose.Schema({
+        name: String,
+        age: Number,
+        height : Number
+    })
+
+    let Student = mongoose.model("students",studentSchema); //create the connection between our schema and the DB
+
+    let newStudent = req.body
+
+    let student = new Student(newStudent)
+
+    student.save().then(
+        ()=>{
+            res.json({
+                message: "Student save successfully"
+            })
+        }
+    ).catch( ()=>{
+        res.json(
+            {
+                message : "Student cannot be saved"
+            }
+        )
+    })
 });
 
 app.delete("/", (req, res)=>{
