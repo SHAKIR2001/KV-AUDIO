3427ea41dfd958b085f642f106229cce0622f721
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-02 01:04:47 +0530

    Add GET and POST request handlers to the Express server

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import express from "express"
 
 let app = express();
 
+app.get("/", (req, res)=>{
+    console.log( "That is a GET request!")
+});
+
+app.post("/", (req, res)=>{
+    console.log("This is a POST request")
+});
+
+
+
 app.listen(3000,()=>{
     console.log("Server is running on PORT 3000")
-})
+});
