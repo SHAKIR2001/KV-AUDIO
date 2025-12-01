15cae574328eb4d1bd43d884982541f3cb80b3b5
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-02 01:31:35 +0530

    Add body-parser dependency and update request handlers in index.js😋

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ import express from "express"
 
 let app = express();
 
+app.use(bodyParser.json());  //idhu app = express in pirahu koduttal wendum aduththa requests(GET,POST,PUT,DELETE) nadakka mun
+
 app.get("/", (req, res)=>{
     console.log( "That is a GET request!")
+    res.json( {
+        message : "Good morning " + req.body.Name
+    })
 });
 
 app.post("/", (req, res)=>{
     console.log("This is a POST request")
+    res.json( {message : "This is a POST request", status : "Success" } )
 });
 
 app.delete("/", (req, res)=>{
-    console.log( "This is DELETE request")
+    console.log( "This is DELETE a request")
 });
 
 
