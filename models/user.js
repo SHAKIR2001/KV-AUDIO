3e243a9d299bfd87e18a2df1a84583df65f7e073
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-17 08:18:20 +0530

    Add user model 😎

---
+++ models/user.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type :String,
+        required : true,
+        unique : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    },
+
+    role : {
+        type : String,
+        default : "customer"
+    },
+
+    firstName : {
+        type : String,
+        required : true
+    },
+
+    lastName : {
+        type : String,
+        require : true
+    },
+
+    address : {
+        type : String,
+        required : true
+    },
+
+    phone :  {
+        type : String,
+        required : true
+    }
+
+});
+
+const userModel = mongoose.model("User",userSchema)
+
+export default userModel
+
