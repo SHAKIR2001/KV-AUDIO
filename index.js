05091bb2a48379d385cf4b1bd9f667c24fce6312
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-17 09:35:55 +0530

    Add user router integration for user API endpoints😎

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js"
 
 
 const app = express();
@@ -16,6 +17,8 @@ connection.once("open", ()=>{
     console.log("MongoDB connected successfully ✅")
 });
 
+
+app.use("/api/users", userRouter)
 
 
 app.listen(3000,()=>{
