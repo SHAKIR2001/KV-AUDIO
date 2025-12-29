18d8581794d030915347f0dc2d2f6e3a81e6693c
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-29 13:31:39 +0530

    Add jsonwebtoken dependency and import in userController😎

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
-import User from "../models/user.js"
+import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsnowebtoken";
 export function registerUser(req,res){
 
     const data = req.body;
