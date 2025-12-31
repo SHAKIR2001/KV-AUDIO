5cac95d861ff8df47be97b9175be3b44312d9a3c
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-31 15:24:28 +0530

    Refactor addReview function to streamline data handling and improve error response 😎

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -11,12 +11,22 @@ export function addReview(req,res){
     const data = req.body;
 
     data.name = req.user.firstName + " " + req.user.lastName; //ingu review podum naparin fistname + last name ei eduththu review il ulla name itku koduththal
-
+    data.profilePicture = req.user.profilePicture;
+    data.email = req.user.emaill;
 
     const newReview = new Review(data)
 
-
-
-
-
+    newReview.save().then(
+        ()=>{
+            res.json({
+                message : "Review added successfully"
+            })
+        }
+    ).catch(
+        (error)=>{
+            res.status(500).json({
+                error : "Review cannot be added"
+            })
+        }
+    )
 }
