1c4afde7111b257cf7cc04ac0fac4ce965626a6b
SHAKIR CASSIM <shakircassim077@gmail.com>
2025-12-31 14:29:00 +0530

    Add review model with schema for email, name, rating, comment, and date 👍

---
+++ models/review.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const reviewSchema =  new mongoose.Schema({
+
+    email : {
+        type : String,
+        required : true,
+        unique : true //one email(user) can have only one review
+    },
+
+    name : {
+        type : String,
+        required : true
+    },
+
+    rating : {
+        type : Number,
+        required : true
+    },
+
+    comment : {
+        type : String,
+        required : true
+    },
+
+    date : {
+        type : Date,
+        required : true,
+        default : Date.now() //get the current date
+    }
+
+
+})
+
+const Review = mongoose.model("Reviews", reviewSchema)
+
+export default Review;
