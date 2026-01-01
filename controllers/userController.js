76942d2def953aee01a4a0b5ff6a94504fe0da04
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-01 11:06:59 +0530

    Add profilePicture to JWT token payload on user login 😢

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,6 +45,7 @@ export function loginUser(req,res){
                         firstName : user.firstName,
                         lastName : user.lastName,
                         email : user.email,
+                        profilePicture : user.profilePicture,
                         role : user.role
                     }, process.env.JWT_SECRET)
                     res.json({
