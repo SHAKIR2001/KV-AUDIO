ec23539f209271b607b7ebaebd058b271611b7d1
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-01 13:21:36 +0530

    Add approveReview route to reviewRouter for handling review approvals 😎

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addReview, getReviews, deleteReview } from "../controllers/reviewController.js";
+import { addReview, getReviews, deleteReview, approveReview } from "../controllers/reviewController.js";
 
 const reviewRouter = express.Router();
 
 reviewRouter.post("/", addReview)
 reviewRouter.get("/", getReviews)
 reviewRouter.delete("/:email", deleteReview)
+reviewRouter.put("/:email", approveReview)
 
 export default reviewRouter;
