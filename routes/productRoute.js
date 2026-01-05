86f3d9b66e079cd078ec5f7d507daacd10ef6fd1
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-05 13:21:11 +0530

    Add updateProducts route to productRouter for updating products 😒

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { addProduct,getProducts } from "../controllers/productController.js"; 
+import { addProduct,getProducts, updateProducts } from "../controllers/productController.js"; 
 
 const productRouter = express.Router();
 
 productRouter.post("/", addProduct)
 productRouter.get("/", getProducts)
-
+productRouter.put("/:key", updateProducts)
 export default productRouter;
