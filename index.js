e8bfb9d63cffa424d0a6078ee7cb92926585f5a8
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-05 15:38:50 +0530

    Fix import path for inquiryRouter and update API endpoint to /api/inquiry 😋

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import reviewRouter from "./routes/reviewRouter.js";
-import inquiryRouter from "./models/inquiryRouter.js";
+import inquiryRouter from "./routes/inquiryRouter.js"
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -43,7 +43,7 @@ connection.once("open", ()=>{
 app.use("/api/users", userRouter)
 app.use("/api/products", productRouter)
 app.use("/api/reviews", reviewRouter)
-app.use("/api/inquiries", inquiryRouter)
+app.use("/api/inquiry", inquiryRouter)
 
 app.listen(3000,()=>{
     console.log("Server is running on PORT 3000 🚀")
