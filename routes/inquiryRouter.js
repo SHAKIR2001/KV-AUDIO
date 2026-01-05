9c5c1e0f395fce8f14d8fc5488b7b81b1b844f74
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-05 17:03:52 +0530

    Add GET endpoint for inquiries in inquiryRouter

--- routes/inquiryRouter.js
+++ routes/inquiryRouter.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { addInquiry } from "../controllers/inquiryController.js"
+import { addInquiry , getInqury} from "../controllers/inquiryController.js"
 
 const inquiryRouter = express.Router();
 
 inquiryRouter.post("/", addInquiry)
+inquiryRouter.get("/", getInqury)
 
 export default inquiryRouter;
 
