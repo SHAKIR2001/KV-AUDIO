bf696f939286baf08f579ea3c6ee5a877d211d6a
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-27 20:36:42 +0530

    Add product import and enhance order processing in addOrder function 😁

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,5 @@
 import Order from "../models/order.js";
+import Product from "../models/product.js";
 
 export async function addOrder(req,res){
     const data = req.body;
@@ -8,7 +9,11 @@ export async function addOrder(req,res){
 
     if ( req.user == null){
         res.status(401).json( {message : "Please login and try again"})
+        return
     }
+
+    orderInfo.email = req.user.email; //user in email eduththal
+
 
     const lastOrder = Order.find().sort({orderId: -1}).limit(1) //get the last order ID
 
@@ -23,6 +28,25 @@ export async function addOrder(req,res){
         const currentOrderNumber = lastOrderNumber + 1 //last orderId in number itku +1 pannudhal 
         const formattedNumber = String(currentOrderNumber).padStart(4, '0') //4digits number manage seidhal (doubt) 0001 0002 0003
         orderInfo.orderId = "ORD" + formattedNumber
+    }
+
+    for(let i=0; i<data.orderedItems.length; i++){ //front endaal anuppiya product = data.orderedItems order seidha productshal
+        try{
+             const product = await Product.findOne({key : data.orderId[i].key}) //import product and search if there any products have related to the key that we get from order
+
+             if (product == null){
+                res.statsu(404).json({message : "Product not found"})
+                return
+             }
+
+             
+
+
+        }catch(e){
+            res.status(500).json({message : "Can not make an order"})
+        }
+       
+       
     }
 
 
