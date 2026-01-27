c57ebdd19f5c990db6418899c4b6ffdfe9b89aa7
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-27 20:15:15 +0530

    order ID assignment logic in addOrder function 😁

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,15 +6,21 @@ export async function addOrder(req,res){
 
     }
 
-    "ORD0001"
-
-    const lastOrder = Order.find().sort
-    try{
     
 
+    const lastOrder = Order.find().sort({orderId: -1}).limit(1) //get the last order ID
 
+    if(lastOrder.length == 0){ //check if there any orderIDs avauilable
+        orderInfo.orderId = "ORD0001" //if not avauilable the "ORD0001" this is the 1st orderId
+    }else{
 
-    }catch(e){
+        const lastOrderId = lastOrder[0].orderId     
+        const lastOrderNumberInString = lastOrderId.replace("ORD", "") //remove the "ORD" from the orderId
+        lastOrderNumber = parseInt(lastOrderNumberInString) //ORD ei remove seidhaalum adhan pin irukkum numbers String il irukkum so adhei Int aha maatrudhal
+        const currentOrderNumber = lastOrderNumber + 1 //last orderId in number itku +1 pannudhal 
+        orderInfo.orderId = "ORD" + (lastOrderNumber + 1) 
+        
+
 
     }
 
