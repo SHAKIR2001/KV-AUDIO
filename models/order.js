0a6cde5dd1b5a299130beb98e14aef1d82d855a5
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-01-27 15:33:20 +0530

    Add order schema for managing rental orders 😊

---
+++ models/order.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+
+    orderId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    orderDate : {
+        type : Date,
+        required : true,
+        dafault: Date.now()
+    },
+
+    orderedItems : { //in here we store the perticuler items details because if we want to provide a bill then the product details in the bill should be in the same details when a ordered cant change the price or other etc.. after the order
+        type : [
+            {
+                product : {
+                    key : {
+                        type : String,
+                        required : true
+                    },
+
+                    name : {
+                        type : String,
+                        required : true
+                    },
+
+                    image : {
+                        type : String,
+                        required : true
+                    },
+
+                        price : {
+                        type : Number,
+                        required : true
+                    }
+                },
+
+                quantity : {
+                    type : Number,
+                    required : true
+                }
+            }
+        ],
+
+        required : true
+    },
+
+    days :  { //in here we store how many days the perticuller item needs ( Because we rent items  not sell)
+        type : Number,
+        required : true
+    },
+
+    startingDate : {
+        type : Date,
+        required : true
+    },
+
+    endingDate : {
+        type : Date,
+        required : true
+    },
+
+    totalAmount : {
+        type : Number,
+        required : true
+    }
+
+
+
+})
